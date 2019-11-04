5e9a6566e75417ef4a60a085d6f405c204fc863c
Aluno <Aluno@PC-39168>
2019-11-04 12:08:27 -0200

    teste-upload

---
+++ src/main/java/br/com/sup/TesteUpload.java
@@ -0,0 +1,13 @@
+package br.com.sup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUpload {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteUpload.class, args);
+	}
+
+}
